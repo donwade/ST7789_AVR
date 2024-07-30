537e495157b355934146c82dd76dc99d1da845e7
don wade <don_wade@rogers.com>
2024-07-30 17:53:28 -0400

    fix use of reserved esp variable name

--- examples/ST7789_lib_Lines/ST7789_lib_Lines.ino
+++ examples/ST7789_lib_Lines/ST7789_lib_Lines.ino
@@ -8,7 +8,7 @@
 
 #define SCR_WD ST7789_TFTWIDTH
 #define SCR_HT ST7789_TFTHEIGHT
-void setup() 
+void setup()
 {
   Serial.begin(9600);
   lcd.init();
@@ -39,9 +39,9 @@ byte lx1[MAX_LINES];
 byte ly0[MAX_LINES];
 byte ly1[MAX_LINES];
 byte curLine=0;
-int x0=40,y0=0;
+int ax0=40,ay0=0;
 int dx0=6,dy0=5;
-int x1=80,y1=40;
+int x1=80,ay1=40;  //y1 and y0 are esp32 pre-typed variables as float
 int dx1=9,dy1=8;
 int cc=0;
 int numLines=12;
@@ -49,27 +49,27 @@ int numLines=12;
 void animLines(int mode)
 {
   uint16_t c;
-  x0+=dx0;
-  y0+=dy0;
+  ax0+=dx0;
+  ay0+=dy0;
   x1+=dx1;
-  y1+=dy1;
-  if(x0>SCR_WD-1) { dx0=-dx0; x0=SCR_WD-1; }
-  if(x0<1) { dx0=-dx0; x0=0; }
-  if(y0>SCR_HT-1) { dy0=-dy0; y0=SCR_HT-1; }
-  if(y0<1) { dy0=-dy0; y0=0; }
+  ay1+=dy1;
+  if(ax0>SCR_WD-1) { dx0=-dx0; ax0=SCR_WD-1; }
+  if(ax0<1) { dx0=-dx0; ax0=0; }
+  if(ay0>SCR_HT-1) { dy0=-dy0; ay0=SCR_HT-1; }
+  if(ay0<1) { dy0=-dy0; ay0=0; }
   if(x1>SCR_WD-1) { dx1=-dx1; x1=SCR_WD-1; }
   if(x1<1) { dx1=-dx1; x1=0; }
-  if(y1>SCR_HT-1) { dy1=-dy1; y1=SCR_HT-1; }
-  if(y1<1) { dy1=-dy1; y1=0; }
+  if(ay1>SCR_HT-1) { dy1=-dy1; ay1=SCR_HT-1; }
+  if(ay1<1) { dy1=-dy1; ay1=0; }
   int i=curLine;
   if(mode&1) lcd.drawLine(lx0[i],ly0[i],lx1[i],ly1[i], 0);
   if(mode&2) lcd.drawLine(SCR_WD-1-lx0[i],ly0[i],SCR_WD-1-lx1[i],ly1[i],0);
   if(mode&4) lcd.drawLine(lx0[i],SCR_HT-1-ly0[i],lx1[i],SCR_HT-1-ly1[i],0);
   if(mode&8) lcd.drawLine(SCR_WD-1-lx0[i],SCR_HT-1-ly0[i],SCR_WD-1-lx1[i],SCR_HT-1-ly1[i],0);
-  lx0[curLine]=x0;
+  lx0[curLine]=ax0;
   lx1[curLine]=x1;
-  ly0[curLine]=y0;
-  ly1[curLine]=y1;
+  ly0[curLine]=ay0;
+  ly1[curLine]=ay1;
   if(++curLine>=numLines) curLine=0;
   for(int i=0;i<numLines;i++) {
     c = lcd.rgbWheel(cc+i*25);
